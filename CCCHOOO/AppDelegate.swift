90823eef8207260f4d6abffce53f80a8cd5efd2d
0xfeedface1993 <318715498@qq.com>
2018-01-23 14:16:28 +0800

    Initial Commit

---
+++ CCCHOOO/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CCCHOOO
+//
+//  Created by virus1993 on 2018/1/23.
+//  Copyright © 2018年 ascp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CCCHOOO/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CCCHOOO
+//
+//  Created by virus1993 on 2018/1/23.
+//  Copyright © 2018年 ascp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
