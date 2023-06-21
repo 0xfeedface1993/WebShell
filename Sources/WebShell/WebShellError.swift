f3d20c01751376e55e19e15c086ce1e90219ecf1
John Corner <virus1993@icloud.com>
2023-06-21 08:52:32 +0800

    移除老版本无效代码

--- Sources/WebShell/WebShellError.swift
+++ Sources/WebShell/WebShellError.swift
@@ -12,14 +12,11 @@ public enum WebShellError: Error {
     public enum RequestReason {
         case emptyRequest
         case invalidateURL(request: URLRequest)
-        case taskCancelled(task: DownStreamDataTask, token: DownStreamDataTask.CancelToken)
     }
     
     public enum ReponseReason {
         case invalidURLResponse(response: URLResponse)
         case invalidHTTPStatusCode(response: HTTPURLResponse)
         case URLSessionError(error: Error)
-        case dataModifyingFailed(task: DownStreamDataTask)
-        case noURLResponse(task: DownStreamDataTask)
     }
 }
