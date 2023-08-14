761c739c4487a681c5d6a69b39fe2fa198a5b53d
John Corner <virus1993@icloud.com>
2023-08-14 16:59:45 +0800

    remove Notification.Name

--- Sources/WebShell/Const.swift
+++ Sources/WebShell/Const.swift
@@ -14,4 +14,4 @@
 
 let userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Safari/605.1.15"
 let fullAccept = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
-public let ImageFetchNotification = Notification.Name("com.ascp.image.code")
+//public let ImageFetchNotification = Notification.Name("com.ascp.image.code")
