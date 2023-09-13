4a8c0cdea94aff8e9f64e12b47e2192c948a16fa
John Corner <virus1993@icloud.com>
2023-09-14 03:41:51 +0800

    test shared URLClient

--- Sources/Durex/URLSessionHolder.swift
+++ Sources/Durex/URLSessionHolder.swift
@@ -21,41 +21,55 @@ struct URLSessionHolder {
     }
     
     init(_ delegator: URLSessionDelegator) {
-        let configure = URLSessionConfiguration.default
-        configure.timeoutIntervalForRequest = 20 * 60
-        configure.timeoutIntervalForResource = 15 * 24 * 3600
-//        let cookies = HTTPCookieStorage()
-//        configure.httpCookieStorage = cookies
-        configure.httpCookieStorage?.removeCookies(since: Date(timeIntervalSinceReferenceDate: 0))
-        configure.httpCookieStorage?.cookieAcceptPolicy = .always
-        configure.httpShouldSetCookies = true
-        
-        self.session = URLSession(configuration: configure, delegate: delegator, delegateQueue: nil)
-        if let storage = configure.httpCookieStorage {
+//        let configure = URLSessionConfiguration.default
+//        configure.timeoutIntervalForRequest = 20 * 60
+//        configure.timeoutIntervalForResource = 15 * 24 * 3600
+////        let cookies = HTTPCookieStorage()
+////        configure.httpCookieStorage = cookies
+//        configure.httpCookieStorage?.removeCookies(since: Date(timeIntervalSinceReferenceDate: 0))
+//        configure.httpCookieStorage?.cookieAcceptPolicy = .always
+//        configure.httpShouldSetCookies = true
+//
+//        self.session = URLSession(configuration: configure, delegate: delegator, delegateQueue: nil)
+//        if let storage = configure.httpCookieStorage {
+//            self.cookies = storage
+//        }   else    {
+//            let message = "empty httpCookieStorage in configure \(configure)."
+//            logger.error(.init(stringLiteral: message))
+//            fatalError(message)
+//        }
+        self.session = URLSession.shared
+        if let storage = URLSession.shared.configuration.httpCookieStorage {
             self.cookies = storage
         }   else    {
-            let message = "empty httpCookieStorage in configure \(configure)."
+            let message = "empty httpCookieStorage in shared \(URLSession.shared)."
             logger.error(.init(stringLiteral: message))
             fatalError(message)
         }
     }
     
     init(_ delegate: any AsyncURLSessiobDownloadDelegate) {
-        let configure = URLSessionConfiguration.default
-        configure.timeoutIntervalForRequest = 20 * 60
-        configure.timeoutIntervalForResource = 15 * 24 * 3600
-//        let cookies = HTTPCookieStorage()
-//        cookies.cookieAcceptPolicy = .always
-//        configure.httpCookieStorage = cookies
-        configure.httpCookieStorage?.removeCookies(since: Date(timeIntervalSinceReferenceDate: 0))
-        configure.httpCookieStorage?.cookieAcceptPolicy = .always
-        configure.httpShouldSetCookies = true
+//        let configure = URLSessionConfiguration.default
+//        configure.timeoutIntervalForRequest = 20 * 60
+//        configure.timeoutIntervalForResource = 15 * 24 * 3600
+//        configure.httpCookieStorage?.removeCookies(since: Date(timeIntervalSinceReferenceDate: 0))
+//        configure.httpCookieStorage?.cookieAcceptPolicy = .always
+//        configure.httpShouldSetCookies = true
+//
+//        self.session = URLSession(configuration: configure, delegate: delegate, delegateQueue: OperationQueue())
+//        if let storage = configure.httpCookieStorage {
+//            self.cookies = storage
+//        }   else    {
+//            let message = "empty httpCookieStorage in configure \(configure)."
+//            logger.error(.init(stringLiteral: message))
+//            fatalError(message)
+//        }
         
-        self.session = URLSession(configuration: configure, delegate: delegate, delegateQueue: nil)
-        if let storage = configure.httpCookieStorage {
+        self.session = URLSession.shared
+        if let storage = URLSession.shared.configuration.httpCookieStorage {
             self.cookies = storage
         }   else    {
-            let message = "empty httpCookieStorage in configure \(configure)."
+            let message = "empty httpCookieStorage in shared \(URLSession.shared)."
             logger.error(.init(stringLiteral: message))
             fatalError(message)
         }
