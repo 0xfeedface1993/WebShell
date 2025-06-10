6e204c067a9d9f258c618e18f067dc62a7327429
virus1993 <virus1993@icloud.com>
2025-06-10 13:37:08 +0800

    add completion flag

--- Sources/AnyErase/AsyncSubject.swift
+++ Sources/AnyErase/AsyncSubject.swift
@@ -15,14 +15,20 @@ actor AsyncSubjectActor {
 fileprivate actor AsyncSubjectHolder<T: Sendable> {
     public typealias Subject = AsyncThrowingStream<T, Error>
     private var continuations = [UUID: Subject.Continuation]()
+    fileprivate var recentValue: T?
+    private var isCompleted = false
     
     public func send(_ value: T) {
+        recentValue = value
         for (_, continuation) in continuations {
             continuation.yield(value)
         }
     }
     
     public func completion(_ error: Error?) {
+        guard !isCompleted else { return }
+        recentValue = nil
+        isCompleted = true
         for (_, continuation) in continuations {
             if let error {
                 continuation.finish(throwing: error)
@@ -34,8 +40,33 @@ fileprivate actor AsyncSubjectHolder<T: Sendable> {
     }
     
     public func subscribe() -> Subject {
-        let uuid = UUID()
-        return Subject { continuation in
+        if isCompleted {
+            return Subject { continuation in
+                continuation.finish()
+            }
+        } else {
+            let uuid = UUID()
+            return Subject { continuation in
+                continuation.onTermination = { [weak self] finished in
+                    guard let self else { return }
+                    Task {
+                        await self.drop(uuid)
+                    }
+                }
+                continuations[uuid] = continuation
+                logger.trace("add continuation for \(uuid), total \(continuations.count) subscribers")
+                if let recentValue {
+                    continuation.yield(recentValue)
+                }
+            }
+        }
+    }
+    
+    public func subscribe(_ continuation: Subject.Continuation) {
+        if isCompleted {
+            return
+        } else {
+            let uuid = UUID()
             continuation.onTermination = { [weak self] finished in
                 guard let self else { return }
                 Task {
@@ -47,27 +78,25 @@ fileprivate actor AsyncSubjectHolder<T: Sendable> {
         }
     }
     
-    public func subscribe(_ continuation: Subject.Continuation) {
-        let uuid = UUID()
-        continuation.onTermination = { [weak self] finished in
-            guard let self else { return }
-            Task {
-                await self.drop(uuid)
-            }
-        }
-        continuations[uuid] = continuation
-        logger.trace("add continuation for \(uuid), total \(continuations.count) subscribers")
-    }
-    
     private func drop(_ id: UUID) {
         logger.trace("remove continuation for \(id), total \(continuations.count) subscribers")
         self.continuations.removeValue(forKey: id)
+    }
+    
+    public func subscribersCount() -> Int {
+        continuations.count
     }
 }
 
 public struct AsyncSubject<T: Sendable>: Sendable {
     public typealias Subject = AsyncThrowingStream<T, Error>
     private var holder = AsyncSubjectHolder<T>()
+    
+    public var currentValue: T? {
+        get async {
+            await holder.recentValue
+        }
+    }
     
     public init() {
         
@@ -91,6 +120,10 @@ public struct AsyncSubject<T: Sendable>: Sendable {
                 await holder.subscribe(continuation)
             }
         }
+    }
+    
+    public func subscribersCount() async -> Int {
+        await holder.subscribersCount()
     }
 }
 
